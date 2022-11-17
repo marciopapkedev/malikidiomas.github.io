af8fb554fecf44e62a58eb1091754e2b79a13891
Márcio Osvaldo Papke <39557454+marciopapke@users.noreply.github.com>
2022-11-17 11:20:15 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "540bbc123052ba0003dc9867659a482d",
-"/": "540bbc123052ba0003dc9867659a482d",
-"main.dart.js": "3b3e2834fca5bed02b5308bd09de74a2",
+"index.html": "36fabcf703c6cb8cf35cb0f1590b036f",
+"/": "36fabcf703c6cb8cf35cb0f1590b036f",
+"main.dart.js": "696e413a1698148db854202b2610fea6",
 "manifest.json": "e04718eff33de79d3b3efa6da5734c5a",
 "version.json": "25ab78238831ec0a9f3bb98183ad1d26"
 };

4b8df71ffb5a94064328f3fe084380ba9544b840
Márcio Osvaldo Papke <39557454+marciopapke@users.noreply.github.com>
2024-02-06 10:49:53 -0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "aa3533898afd3dcd7933d5be894f4d63",
-"index.html": "8c2210915466da8566ee22405a440c92",
-"/": "8c2210915466da8566ee22405a440c92",
-"main.dart.js": "5c77ab2fb391b7c183dcf8b71dfdbc53",
+"index.html": "1b3eaa49d57179cc4e06b49f5c604beb",
+"/": "1b3eaa49d57179cc4e06b49f5c604beb",
+"main.dart.js": "3b11a1ee0a724474d9dd9781f61abef9",
 "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
